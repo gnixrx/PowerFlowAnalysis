ee6545e849007074bda47f7f4f0a9027ecbaac9e
Ethan Widger <gnixrx@gmail.com>
2023-11-18 19:12:21 -0800

    Added generator and load properties.

--- data_bus.py
+++ data_bus.py
@@ -15,29 +15,72 @@ The BusData class describes the attributes of the busses in the power system.
 :type p_gen: int
 :param v_set: Voltage set in pu.
 :type v_set: float
-
-TODO:
-Create the bus variables and mismatch implicit equations
 """
 
 class BusData:
+    ### ----------------------------------------- Getters -----------------------------------------
+    @property
+    def id(self):
+        """
+        Returns the bus number
+        :return: Bus Number
+        :rtype: int
+        """
+        return self._b
+
     @property
     def P(self):
         """
-        Returns the active power at this bus in pu.
+        Returns the active power at this bus.
         :return: Active power
         :rtype: float
         """
         return self._P
 
     @property
+    def P_gen(self):
+        """
+        Returns how much power is generated at this bus.
+        :return: Active power generated at this bus.
+        :rtype: float
+        """
+        return self.P + self._p_load
+
+    @property
+    def P_load(self):
+        """
+        Returns the load at this bus.
+        :return: Active power load at this bus.
+        :rtype: float
+        """
+        return self._p_load
+
+    @property
     def Q(self):
         """
-        Returns the reactive power at this bus in pu.
+        Returns the reactive power at this bus.
         :return: Reactive power
         :rtype: float
         """
         return self._Q
+
+    @property
+    def Q_gen(self):
+        """
+        Returns the reactive power generated at this bus.
+        :return: Reactive power
+        :rtype: float
+        """
+        return self.Q + self._q_load
+
+    @property
+    def Q_load(self):
+        """
+        Returns the reactive power load at this bus.
+        :return: Reactive power
+        :rtype: float
+        """
+        return self._q_load
 
     @property
     def V(self):
@@ -57,6 +100,16 @@ class BusData:
         """
         return self._Th
 
+    @property
+    def type(self):
+        """
+        Returns the type of this bus, Slack, PV, or PQ.
+        :return: Type of bus
+        :rtype: string
+        """
+        return self._type_verbose
+
+    ### ----------------------------------------- Setters -----------------------------------------
     @P.setter
     def P(self, p):
         if self._type == 'S': # only settable in S bus
@@ -77,21 +130,20 @@ class BusData:
         if self._type != 'S': # only settable in a PQ or PV bus
             self._Th = th
 
+    ### ----------------------------------------- Functions -----------------------------------------
 
     def __init__(self, b: int, p: float, q: float, type: chr, p_gen: int, v_set: float):
         self._b = b - 1 # Bus Number index
         self._type = type # Bus type
-
         # Create verbose version of type
-        if self._type == 'S': self._type_verbose = "Slack bus"
-        elif self._type == 'G': self._type_verbose = "PV bus"
-        else: self._type_verbose = "PQ bus"
+        if self._type == 'S': self._type_verbose = "Slack"
+        elif self._type == 'G': self._type_verbose = "PV"
+        else: self._type_verbose = "PQ"
 
         self._p_load = p / s_base # Load Active Power in pu
         self._q_load = q / s_base # Load Reactive Power in pu
         self._p_gen = p_gen / s_base # Generated Active Power in pu
-
-        self._v_set = v_set # Reference voltage at the bus in pu
+        self._v_set = v_set # Voltage at the bus in pu
 
         self._P = None
         if self._type == 'G' or self._type == 'D':
@@ -101,19 +153,14 @@ class BusData:
         if self._type == 'D':
             self._Q = -self._q_load
 
-        self._V = 1.0 # Initial Guess V in pu
+        self._V = None
         if self._type == 'S' or self._type == 'G':
             self._V = self._v_set
 
-        self._Th = 0.0 # Initial guess or set for Slack Bus in radians
+        self._Th = None
+        if self._type == 'S':
+            self._Th = 0
 
 
     def __repr__(self):
-        if self._P == None: p = "Unknown"
-        else: p = f"{np.round(self._P, 3)} p.u., {np.round(self._P * s_base, 3)} MW"
-
-        if self._Q == None: q = "Unknown"
-        else: q = f"{np.round(self._Q, 3)} p.u., {np.round(self._Q * s_base, 3)} MVar"
-
-        return (f"{self.__class__.__name__}> Bus Num: {self._b + 1}, Type: {self._type_verbose}, "
-                f"P: {p},  Q: {q}, V: {np.round(self._V, 3)} p.u., Theta: {np.round(np.degrees(self._Th), 3)}\xb0")
+        return (f"{self.__class__.__name__}> Bus: {self._b + 1}, Type: {self.type}")
